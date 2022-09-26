0d6d4c18b2dddca9f18fb0f03fb89139cb98e2d2
Agnesh Radhakrishnan <agnesh@Agneshs-MacBook-Air.local>
2022-09-26 12:09:59 +0530

    added api documentation

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    'dj_rest_auth.registration'
+    'dj_rest_auth.registration',
+    'drf_spectacular'
 ]
 
 REST_FRAMEWORK={
@@ -57,7 +58,14 @@ REST_FRAMEWORK={
     "DEFAULT_AUTHENTICATION_CLASSES":[
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication"
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS":"drf_spectacular.openapi.AutoSchema"
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A blog api to learn more about django rest framework",
+    "VERSION": "0.9.0",
+    # OTHER SETTINGS
 }
 
 MIDDLEWARE = [
